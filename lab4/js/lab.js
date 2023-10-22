86118f2dd6fa32ee0f4e8612235eb06dfeadd0cc
nicolevfk <146919679+nicolevfk@users.noreply.github.com>
2023-10-22 16:09:31 -0700

    lab 4 completed update

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,16 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Create Pseudocode for Go Fish
+// Author: Nicole Van Fleet Kingery
+// Date: 10/21/2023
 
-// Constants
+// Load players with 7 cards
+	// Randomly load cards from the deck
+		// Each card delt is removed from the deck
+// First player clicks card in hand wanting to be matched
+	// Check if the other player has a matching card
+// If yes: give to player 
+// If no: “go fish” message
+// Allow player to draw from random pile
+// Other player’s turn
+// Check for win condition
+// Win condition: when all cards in possession are pairs, player with most pairs wins
 
-// Functions
-
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
