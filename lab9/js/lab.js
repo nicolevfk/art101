4cb374c1d382a67c49e27f96251258bd6a0ddc5c
nicolevfk <146919679+nicolevfk@users.noreply.github.com>
2023-11-12 14:17:25 -0800

    lab 9 update

---
+++ lab9/js/lab.js
@@ -0,0 +1,12 @@
+// index.js - Libraries and jQuery
+// Author: Nicole Van Fleet Kingery
+// Date: 11/7/23
+
+// create a button for each div
+$(".minor-section").append("<br><button class='toggle-css'>Make Special</button>");
+
+//make buttons do functions when clicked
+    // when clicking the "Make Special" button, the section should change css
+$(".minor-section button").click( function() {
+    ($(this).parent()).toggleClass("special");
+});
