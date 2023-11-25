deb967860e7dfae84098299af54fea86894db510
nicolevfk <146919679+nicolevfk@users.noreply.github.com>
2023-11-24 21:07:12 -0800

    lab 13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -4,22 +4,49 @@
     Date: 11/22/23
 */
 
-function FizzBuzz() {
-    for (var n = 0; n <= 120; n++) {
-        word = ""
-        if (n % 3 == 0) {
-            word = "Fizz";
+// get values and sort them in a map with {num: word} for every one where both exist (used some code from Wes' page)
+function getFactors() {
+    var factors = {};
+    for (factor = 1; factor <= 4; factor++) {
+        numId = "num" + factor;
+        wordId = "w" + factor;
+        numValue = document.getElementById(numId).value;
+        wordValue = document.getElementById(wordId).value;
+        if (numValue && wordValue) {
+            factors[numValue] = wordValue;
         }
-        if (n % 5 == 0) {
-            word = word + "Buzz";
+    }
+    console.log(factors)
+    return factors
+}
+
+
+function FizzBuzz(factors) {
+    maxValue = document.getElementById("max").value;
+    console.log(maxValue);
+    for (var n = 0; n <= maxValue; n++) {
+        var word = "";
+        // factors is {number: text}
+        // for every number in factors, it checks to see if it's divisible by the current n, and 
+        // if it is, it attaches the text associated with the number to the current string var "word"
+        for (i in factors) {
+            if (n % i == 0) {
+                word += factors[i];
+            }
         }
-        if (n % 7 == 0) {
-            word = word + "Boom";
+        // if it's the first item, it uses html, otherwise it appends the word value
+        if (n == 0) {
+            $("#output").html(n + " - " + word + "!</br>");
+        } else if (word.length > 0) {
+            $("#output").append(n + " - " + word + "!</br>");
+        } else {
+            $("#output").append(n + "</br>");
         }
-        if (word.length > 0) {
-            word = word + "!";
-        }
-        $("#output").append(n + " - " + word + "</br>")
     }
 }
 
+// get input from input fields when button is clicked
+$("#button").click(function() {
+    // get inputs and run FizzBuzz function
+    FizzBuzz(getFactors());
+});
